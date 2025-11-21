5be903d9839869c6f10b0b048ae7e56d6c4e4722
Swarnavo2003 <swarnavo2020@gmail.com>
2025-11-21 10:56:39 +0530

    feat: project initialization and category controller setup

---
+++ src/main/java/in/swarnavo/ecommerce/controller/CategoryController.java
@@ -0,0 +1,27 @@
+package in.swarnavo.ecommerce.controller;
+
+import in.swarnavo.ecommerce.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    private final List<Category> categories = new ArrayList<>();
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories() {
+        return categories;
+    }
+
+    @PostMapping("/api/admin/category")
+    public String createCategory(@RequestBody Category category) {
+        categories.add(category);
+        return "Category Added Successfully";
+    }
+}

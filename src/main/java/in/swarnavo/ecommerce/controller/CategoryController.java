2afc99bf1b576515c11aba4b6a8ad75304b22690
Swarnavo2003 <swarnavo2020@gmail.com>
2025-11-22 00:39:59 +0530

    feat: deletion of category added

--- src/main/java/in/swarnavo/ecommerce/controller/CategoryController.java
+++ src/main/java/in/swarnavo/ecommerce/controller/CategoryController.java
@@ -2,10 +2,7 @@ package in.swarnavo.ecommerce.controller;
 
 import in.swarnavo.ecommerce.model.Category;
 import in.swarnavo.ecommerce.service.CategoryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,5 +25,11 @@ public class CategoryController {
     public String createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
         return "Category Added Successfully";
+    }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public String deleteCategory(@PathVariable Long categoryId) {
+        String status = categoryService.deleteCategory(categoryId);
+        return status;
     }
 }

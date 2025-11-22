60601604b06e82c267c33412fd3e3812515d452d
Swarnavo2003 <swarnavo2020@gmail.com>
2025-11-22 09:41:40 +0530

    feat: update category endpoint added

--- src/main/java/in/swarnavo/ecommerce/controller/CategoryController.java
+++ src/main/java/in/swarnavo/ecommerce/controller/CategoryController.java
@@ -39,4 +39,14 @@ public class CategoryController {
             return new ResponseEntity<>(e.getReason(), e.getStatusCode());
         }
     }
+
+    @PutMapping("/api/admin/categories/{categoryId}")
+    public ResponseEntity<String> updateCategory(@PathVariable Long categoryId, @RequestBody Category category) {
+        try {
+            Category status = categoryService.updateCategory(categoryId, category);
+            return new ResponseEntity<>("Category updated with categoryId " + categoryId, HttpStatus.OK);
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
+    }
 }

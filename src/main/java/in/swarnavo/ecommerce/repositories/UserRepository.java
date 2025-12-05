d15b6f0a4dec9d97a30bfb653baedf8043432695
Swarnavo2003 <swarnavo2020@gmail.com>
2025-12-05 19:15:31 +0530

    feat: user repository added and user details service implemention class configured

---
+++ src/main/java/in/swarnavo/ecommerce/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package in.swarnavo.ecommerce.repositories;
+
+import in.swarnavo.ecommerce.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
---
+++ src/main/java/in/swarnavo/ecommerce/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package in.swarnavo.ecommerce.security.services;
+
+import in.swarnavo.ecommerce.model.User;
+import in.swarnavo.ecommerce.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}

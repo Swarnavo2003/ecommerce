6bc1f30c2a9f20d181be7bbe70a13da41d406f03
Swarnavo2003 <swarnavo2020@gmail.com>
2025-12-06 12:08:34 +0530

    feat: few minor tweaks and auth controller testing

--- src/main/java/in/swarnavo/ecommerce/security/WebSecurityConfig.java
+++ src/main/java/in/swarnavo/ecommerce/security/WebSecurityConfig.java
@@ -1,23 +1,31 @@
 package in.swarnavo.ecommerce.security;
 
+import in.swarnavo.ecommerce.model.AppRole;
+import in.swarnavo.ecommerce.model.Role;
+import in.swarnavo.ecommerce.model.User;
+import in.swarnavo.ecommerce.repositories.RoleRepository;
+import in.swarnavo.ecommerce.repositories.UserRepository;
 import in.swarnavo.ecommerce.security.jwt.AuthEntryPoint;
 import in.swarnavo.ecommerce.security.jwt.AuthTokenFilter;
 import in.swarnavo.ecommerce.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Set;
 
 @Configuration
 @EnableWebSecurity
@@ -66,15 +74,19 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(auth ->
                     auth.requestMatchers("/api/auth/**").permitAll()
                             .requestMatchers("/v3/api-docs/**").permitAll()
+                            .requestMatchers("/h2-console/**").permitAll()
                             .requestMatchers("/swagger-ui/**").permitAll()
-                            .requestMatchers("/api/public/**").permitAll()
-                            .requestMatchers("/api/admin/**").permitAll()
+                            //.requestMatchers("/api/public/**").permitAll()
+                            //.requestMatchers("/api/admin/**").permitAll()
                             .requestMatchers("/api/test/**").permitAll()
                             .requestMatchers("/images/**").permitAll()
                             .anyRequest().authenticated());
 
         http.authenticationProvider(authenticationProvider());
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.headers(header -> header.frameOptions(
+                HeadersConfigurer.FrameOptionsConfig::sameOrigin
+        ));
         return http.build();
     }
 
@@ -88,5 +100,63 @@ public class WebSecurityConfig {
                 "/swagger-ui.html",
                 "/webjars/**"
         ));
+    }
+
+    @Bean
+    public CommandLineRunner initData(RoleRepository roleRepository, UserRepository userRepository) {
+        return args -> {
+            Role userRole = roleRepository.findByRoleName(AppRole.ROLE_USER)
+                    .orElseGet(() -> {
+                        Role newUserRole = new Role(AppRole.ROLE_USER);
+                        return roleRepository.save(newUserRole);
+                    });
+
+            Role sellerRole = roleRepository.findByRoleName(AppRole.ROLE_SELLER)
+                    .orElseGet(() -> {
+                        Role newSellerRole = new Role(AppRole.ROLE_SELLER);
+                        return roleRepository.save(newSellerRole);
+                    });
+
+            Role adminRole = roleRepository.findByRoleName(AppRole.ROLE_ADMIN)
+                    .orElseGet(() -> {
+                        Role newAdminRole = new Role(AppRole.ROLE_ADMIN);
+                        return roleRepository.save(newAdminRole);
+                    });
+
+            Set<Role> userRoles = Set.of(userRole);
+            Set<Role> sellerRoles = Set.of(sellerRole);
+            Set<Role> adminRoles = Set.of(userRole, sellerRole, adminRole);
+
+            if(!userRepository.existsByUsername("user1")) {
+                User user1 = new User("user1", "user1@example.com", passwordEncoder().encode("password1"));
+                userRepository.save(user1);
+            }
+
+            if (!userRepository.existsByUsername("seller1")) {
+                User seller1 = new User("seller1", "seller1@example.com", passwordEncoder().encode("password2"));
+                userRepository.save(seller1);
+            }
+
+            if (!userRepository.existsByUsername("admin")) {
+                User admin = new User("admin", "admin@example.com", passwordEncoder().encode("adminPass"));
+                userRepository.save(admin);
+            }
+
+            userRepository.findByUsername("user1").ifPresent(user -> {
+                user.setRoles(userRoles);
+                userRepository.save(user);
+            });
+
+            userRepository.findByUsername("seller1").ifPresent(seller -> {
+                seller.setRoles(sellerRoles);
+                userRepository.save(seller);
+            });
+
+            userRepository.findByUsername("admin").ifPresent(admin -> {
+                admin.setRoles(adminRoles);
+                userRepository.save(admin);
+            });
+
+        };
     }
 }

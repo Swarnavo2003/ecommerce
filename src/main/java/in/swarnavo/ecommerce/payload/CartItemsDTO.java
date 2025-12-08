2bc5a1da89c2fe6620293c78fdadc84bf00cdfc5
Swarnavo2003 <swarnavo2020@gmail.com>
2025-12-08 18:06:32 +0530

    feat: cart and cart item dto added

---
+++ src/main/java/in/swarnavo/ecommerce/payload/CartItemsDTO.java
@@ -0,0 +1,17 @@
+package in.swarnavo.ecommerce.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItemsDTO {
+    private Long cartItemId;
+    private CartDTO cartDTO;
+    private ProductDTO productDTO;
+    private Integer quantity;
+    private Double discount;
+    private Double productPrice;
+}

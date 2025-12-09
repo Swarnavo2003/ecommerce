bfd392534f3b9662925b57e4770b7f0d1afea2ac
Swarnavo2003 <swarnavo2020@gmail.com>
2025-12-09 13:04:44 +0530

    feat: get all carts api(admin) endpoint done

--- src/main/java/in/swarnavo/ecommerce/service/CartService.java
+++ src/main/java/in/swarnavo/ecommerce/service/CartService.java
@@ -2,6 +2,10 @@ package in.swarnavo.ecommerce.service;
 
 import in.swarnavo.ecommerce.payload.CartDTO;
 
+import java.util.List;
+
 public interface CartService {
     CartDTO addProductToCart(Long productId, Integer quantity);
+
+    List<CartDTO> getAllCarts();
 }

1033483c0b338a7e12299305ea898fa7647fc02a
Swarnavo2003 <swarnavo2020@gmail.com>
2025-12-19 17:41:59 +0530

    feat: order repository added

---
+++ src/main/java/in/swarnavo/ecommerce/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package in.swarnavo.ecommerce.repositories;
+
+import in.swarnavo.ecommerce.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}

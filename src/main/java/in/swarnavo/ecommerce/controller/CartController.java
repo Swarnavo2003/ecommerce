dcfe32ded7bcd0fc144129567662c2fc4e6bad98
Swarnavo2003 <swarnavo2020@gmail.com>
2025-12-21 15:35:12 +0530

    feat: swagger annotations added to cart and category api

--- src/main/java/in/swarnavo/ecommerce/controller/CartController.java
+++ src/main/java/in/swarnavo/ecommerce/controller/CartController.java
@@ -5,6 +5,9 @@ import in.swarnavo.ecommerce.payload.CartDTO;
 import in.swarnavo.ecommerce.repositories.CartRepository;
 import in.swarnavo.ecommerce.service.CartService;
 import in.swarnavo.ecommerce.util.AuthUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,21 +28,27 @@ public class CartController {
     @Autowired
     private CartRepository cartRepository;
 
+    @Tag(name = "Cart APIs", description = "APIs for managing carts")
+    @Operation(summary = "Add Product To Cart", description = "API to add products to cart")
     @PostMapping("/carts/products/{productId}/quantity/{quantity}")
     public ResponseEntity<CartDTO> addProductToCart(
-            @PathVariable Long productId,
-            @PathVariable Integer quantity)
+            @Parameter(description = "ID of the product that you want to add to cart") @PathVariable Long productId,
+            @Parameter(description = "Quantity of product that you want to add to cart") @PathVariable Integer quantity)
     {
         CartDTO cartDTO = cartService.addProductToCart(productId, quantity);
         return new ResponseEntity<>(cartDTO, HttpStatus.CREATED);
     }
 
+    @Tag(name = "Cart APIs", description = "APIs for managing carts")
+    @Operation(summary = "Get All Carts", description = "API to get all carts")
     @GetMapping("/carts")
     public ResponseEntity<List<CartDTO>> getCarts() {
         List<CartDTO> cartDTOS = cartService.getAllCarts();
         return new ResponseEntity<>(cartDTOS, HttpStatus.FOUND);
     }
 
+    @Tag(name = "Cart APIs", description = "APIs for managing carts")
+    @Operation(summary = "Get User Carts", description = "API to get user cart")
     @GetMapping("/carts/user/cart")
     public ResponseEntity<CartDTO> getCartById() {
         String emailId = authUtil.loggedInEmail();
@@ -49,6 +58,8 @@ public class CartController {
         return new ResponseEntity<>(cartDTO, HttpStatus.OK);
     }
 
+    @Tag(name = "Cart APIs", description = "APIs for managing carts")
+    @Operation(summary = "Update Products In Cart", description = "API to update products in cart")
     @PutMapping("/cart/products/{productId}/quantity/{operation}")
     public ResponseEntity<CartDTO> updateCartProduct(
             @PathVariable Long productId,
@@ -59,6 +70,8 @@ public class CartController {
         return new ResponseEntity<>(cartDTO, HttpStatus.OK);
     }
 
+    @Tag(name = "Cart APIs", description = "APIs for managing carts")
+    @Operation(summary = "Delete Products From Cart", description = "API to delete products from carts")
     @DeleteMapping("/carts/{cartId}/product/{productId}")
     public ResponseEntity<String> deleteProductFromCart(
             @PathVariable Long cartId,
--- src/main/java/in/swarnavo/ecommerce/controller/CategoryController.java
+++ src/main/java/in/swarnavo/ecommerce/controller/CategoryController.java
@@ -4,6 +4,12 @@ import in.swarnavo.ecommerce.config.AppConstants;
 import in.swarnavo.ecommerce.payload.CategoryDTO;
 import in.swarnavo.ecommerce.payload.CategoryResponse;
 import in.swarnavo.ecommerce.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,6 +23,14 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
+    @Tag(name = "Category APIs", description = "APIs for managing categories")
+    @Operation(summary = "Fetch Categories", description = "API to fetch all categories")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "All categories fetched successfully"),
+            @ApiResponse(responseCode = "400", description = "No categories found"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+
+    })
     @GetMapping("/public/categories")
     public ResponseEntity<CategoryResponse> getAllCategories(
             @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER) Integer pageNumber,
@@ -27,20 +41,44 @@ public class CategoryController {
         return ResponseEntity.ok(categoryService.getAllCategories(pageNumber, pageSize, sortBy, sortOrder));
     }
 
+    @Tag(name = "Category APIs", description = "APIs for managing categories")
+    @Operation(summary = "Create Category", description = "API to create a new category")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Category is created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid Input", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+
+    })
     @PostMapping("/admin/categories")
     public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO) {
         CategoryDTO savedCategory = categoryService.createCategory(categoryDTO);
         return new ResponseEntity<>(savedCategory, HttpStatus.CREATED);
     }
 
+    @Tag(name = "Category APIs", description = "APIs for managing categories")
+    @Operation(summary = "Delete Category", description = "API to delete a category")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Category deleted successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid Input", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+
+    })
     @DeleteMapping("/admin/categories/{categoryId}")
-    public ResponseEntity<CategoryDTO> deleteCategory(@PathVariable Long categoryId) {
+    public ResponseEntity<CategoryDTO> deleteCategory(@Parameter(description = "ID of the category that you want to delete") @PathVariable Long categoryId) {
         CategoryDTO deleteCategory = categoryService.deleteCategory(categoryId);
         return new ResponseEntity<>(deleteCategory, HttpStatus.OK);
     }
 
+    @Tag(name = "Category APIs", description = "APIs for managing categories")
+    @Operation(summary = "Update Category", description = "API to update a category")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Category updated successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid Input", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+
+    })
     @PutMapping("/admin/categories/{categoryId}")
-    public ResponseEntity<CategoryDTO> updateCategory(@PathVariable Long categoryId,@Valid @RequestBody CategoryDTO categoryDTO) {
+    public ResponseEntity<CategoryDTO> updateCategory(@Parameter(description = "ID of the category that you want to update") @PathVariable Long categoryId,@Valid @RequestBody CategoryDTO categoryDTO) {
         CategoryDTO savedCategory = categoryService.updateCategory(categoryId, categoryDTO);
         return new ResponseEntity<>(savedCategory, HttpStatus.OK);
     }

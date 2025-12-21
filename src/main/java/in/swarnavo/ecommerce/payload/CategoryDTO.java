f59e8808cb5dc0dfb7ce218ad3a4961e1c587258
Swarnavo2003 <swarnavo2020@gmail.com>
2025-12-21 17:02:51 +0530

    feat: scham annotation added to category dto to add description to fields

--- src/main/java/in/swarnavo/ecommerce/payload/CategoryDTO.java
+++ src/main/java/in/swarnavo/ecommerce/payload/CategoryDTO.java
@@ -1,5 +1,6 @@
 package in.swarnavo.ecommerce.payload;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryDTO {
+    @Schema(description = "Category ID", example = "101")
     private Long categoryId;
+
+    @Schema(description = "Category name for category you want to create", name = "iPhone 17")
     private String categoryName;
 }

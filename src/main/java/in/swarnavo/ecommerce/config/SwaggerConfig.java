c2f0aa4b7b0b7a61d892ed4fca034f1b0287b14c
Swarnavo2003 <swarnavo2020@gmail.com>
2025-12-21 16:26:29 +0530

    feat: swagger documentation customization with custom title description and other stuff

--- src/main/java/in/swarnavo/ecommerce/config/SwaggerConfig.java
+++ src/main/java/in/swarnavo/ecommerce/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package in.swarnavo.ecommerce.config;
 
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +26,21 @@ public class SwaggerConfig {
                 .addList("Bearer Authentication");
 
         return new OpenAPI()
+                .info(new Info()
+                        .title("Spring Boot eCommerce API")
+                        .version("1.0")
+                        .description("This is a Spring Boot Project for eCommerce")
+                        .license(new License().name("Apache 2.0").url("http://url.com"))
+                        .contact(new Contact()
+                                .name("Swarnavo Majumder")
+                                .email("mswarnavo2003@gmail.com")
+                                .url("https://github.com/Swarnavo2003")
+                        )
+                )
+                .externalDocs(new ExternalDocumentation()
+                        .description("Project Documentations")
+                        .url("https://github.com/Swarnavo2003")
+                )
                 .components(new Components()
                         .addSecuritySchemes("Bearer Authentication", bearerSchema))
                 .addSecurityItem(bearerRequirement);
